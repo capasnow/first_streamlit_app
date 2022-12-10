eeceee687be081d044174a2b3744f1ed7ed842e9
Diana Soria <120283368+capasnow@users.noreply.github.com>
2022-12-10 17:58:56 -0500

    Update streamlit_app.py
    Textos adicionales

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title("My Parents new Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard Boiled Free Range Egg)
