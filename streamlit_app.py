6cdea46ec2df89dc286c0c6aef814630c9eaaa0c
Diana Soria <120283368+capasnow@users.noreply.github.com>
2022-12-10 17:59:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title("My Parents new Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard Boiled Free Range Egg)
+streamlit.text("Hard Boiled Free Range Egg")
